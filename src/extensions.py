c9044d80646e21aec52459ef71a7baa07081e3aa
Daniel Joris <d.joris@rioanalytics.com>
2025-08-14 00:37:55 -0300

    Add the Circuit Breaker Specification class to implement circuit breaker logic, allowing fault control and recovery in specifications.

--- src/extensions.py
+++ src/extensions.py
@@ -1,3 +1,6 @@
+import time
+from typing import TypeVar
+
 from src.base import Specification
 
 
@@ -40,3 +43,39 @@ class MaxLengthSpecification(Specification[str]):
 
     def is_satisfied_by(self, candidate: str) -> bool:
         return len(candidate) <= self.max_length
+
+
+T = TypeVar("T")
+
+
+class CircuitBreakerSpecification(Specification[T]):
+    def __init__(
+        self, spec: Specification[T], failure_threshold: int, recover_timeout: float = 60
+    ):
+        self.spec = spec
+        self.failure_threshold = failure_threshold
+        self.recover_timeout = recover_timeout
+        self.failure_count = 0
+        self.last_failure_time = 0
+
+    def is_satisfied_by(self, candidate: T) -> bool:
+
+        if self.failure_count >= self.failure_threshold:
+            if time.time() - self.last_failure_time < self.recover_timeout:
+                return False
+            self.reset()
+
+        try:
+            result = self.spec.is_satisfied_by(candidate)
+            if not result:
+                self.failure_count += 1
+                self.last_failure_time = time.time()
+            return result
+        except Exception:
+            self.failure_count += 1
+            self.last_failure_time = time.time()
+            return False
+
+    def reset(self):
+        self.failure_count = 0
+        self.last_failure_time = 0
