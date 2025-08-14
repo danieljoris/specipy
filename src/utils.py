cc4111757026fddb01737b44bd53d97e7ef64b7c
Daniel Joris <d.joris@rioanalytics.com>
2025-08-14 00:37:29 -0300

    Updates type annotations in the filter_by_spec and lambda_spec functions to ensure compatibility with generic types.

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,17 @@
-from typing import Any, Callable, Iterable, Iterator, TypeVar
+from typing import Callable, Iterable, Iterator, TypeVar
 
 from src.base import Specification
 
 T = TypeVar("T")
 
 
-def filter_by_spec(items: Iterable[T], spec: Specification) -> Iterator[T]:
+def filter_by_spec(items: Iterable[T], spec: Specification[T]) -> Iterator[T]:
     return (item for item in items if spec.is_satisfied_by(item))
 
 
-def lambda_spec(fn: Callable[[Any], bool]) -> Specification:
-    class _LambdaSpec(Specification):
-        def is_satisfied_by(self, candidate: Any) -> bool:
+def lambda_spec(fn: Callable[[T], bool]) -> Specification[T]:
+    class _LambdaSpec(Specification[T]):
+        def is_satisfied_by(self, candidate: T) -> bool:
             return fn(candidate)
 
     return _LambdaSpec()
